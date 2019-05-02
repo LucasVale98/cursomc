92747df333557b8e40daf6385743936ae94d7a36
Lucas Vale <lucasvale1998.lv@gmail.com>
2019-05-01 21:17:47 -0300

    Primeira classe Categoria

--- src/main/java/com/cursomc/cursomc/resources/CategoriaResourses.java
+++ src/main/java/com/cursomc/cursomc/resources/CategoriaResourses.java
@@ -1,15 +1,25 @@
 package com.cursomc.cursomc.resources;
 
+import java.util.*;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.cursomc.cursomc.domain.Categoria;
+
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResourses {
-	
+	 
 	@RequestMapping(method=RequestMethod.GET)
-	private String listar() {
-		return "Rest Esta Funcionando";
+	private List<Categoria> listar() {
+		Categoria categoria1 = new Categoria(1, "informatica");
+		Categoria categoria2 = new Categoria(2, "Informaçao");
+		
+		List<Categoria> lista = new  ArrayList<>();
+		lista.add(categoria1);
+		lista.add(categoria2);
+		return lista;
 	}
 }
